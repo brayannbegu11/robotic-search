ad584eb20527f90a8f419b0a887f21c5cb335c02
brayannbegu11 <brayannbegu@unisabana.edu.co>
2022-08-31 14:41:03 -0500

    Updates

---
+++ precache-manifest.94c113b86f5f1b122a916c988c43571c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a58bbbd25606c0f7f408",
+    "url": "/robotic-search/static/css/main.0b6f6017.chunk.css"
+  },
+  {
+    "revision": "a58bbbd25606c0f7f408",
+    "url": "/robotic-search/static/js/main.0f2cad3d.chunk.js"
+  },
+  {
+    "revision": "c97aa5dd286c54551d6a",
+    "url": "/robotic-search/static/js/runtime~main.bc8a1d16.js"
+  },
+  {
+    "revision": "50acd8a27348a2082e7f",
+    "url": "/robotic-search/static/css/2.288dc2c0.chunk.css"
+  },
+  {
+    "revision": "50acd8a27348a2082e7f",
+    "url": "/robotic-search/static/js/2.cf8dcc85.chunk.js"
+  },
+  {
+    "revision": "b64634c5ceb9828e70035a9566934d0c",
+    "url": "/robotic-search/index.html"
+  }
+];
